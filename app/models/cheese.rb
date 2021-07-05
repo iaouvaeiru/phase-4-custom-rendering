3d5ab4b3914da8c36a3d9f1b10275ac64314429f
Leon Fan <leonkind@gmail.com>
2021-07-05 18:39:03 -0500

    commit tree c8c6cf078d62e346e58c193c00fc584e73a52701

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -3,5 +3,19 @@ class Cheese < ApplicationRecord
   def summary
     "#{name}: $#{price}"
   end
+  
+  def index
+    cheeses = Cheese.all
+    render json: cheeses
+  end
+  
+  def show
+    cheese = Cheese.find(params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {error: '404 cheese not found'}
+    end
+  end
 
 end
